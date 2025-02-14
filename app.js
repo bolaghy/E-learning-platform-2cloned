1f82eb8b40189e1c7eaf13e4fc5e6804c50d2eeb
bolaghy <bolajiisaac10@yahoo.com>
2025-02-14 10:52:17 +0100

    last changes

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ const express = require('express');
 const { mongoose } = require('mongoose');
 const courseRouter = require('./Routers/courseRouter')
 const authRouter = require('./Routers/authRouter')
-//const authMiddleware = require('./Middleware/authMiddleware')
 const PORT = process.env.port || 3000
 const api = process.env.api
 
 const app = express()
 
-  
+ 
 app.use(express.json()); // Parses JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parses URL-encoded bodies
 
+// Mount the courseRouter and authRouter at the base API path
+app.use(`${api}`,  courseRouter)      
+app.use(`${api}`,  authRouter)       
 
-app.use(`${api}`,  courseRouter)
-app.use(`${api}`,  authRouter)
-
-
+// Async function to start the server and connect to the database
 const start = async ()=> {
     try {
         await mongoose.connect(process.env.dataBaseString)
